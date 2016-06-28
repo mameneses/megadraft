6666eb72a556f49796d31a1359cc39decf1320a2
Marcelo Jorge Vieira <metal@alucinados.com>
2016-06-27 22:38:55 -0300

    Media: Allowed media featured options customizations

--- src/components/plugin/CommonBlock.js
+++ src/components/plugin/CommonBlock.js
@@ -12,6 +12,10 @@ import {
   BlockControls,
   BlockWrapper
 } from "../../components/plugin";
+import {
+  DEFAULT_FEATURED_OPTIONS,
+  DEFAULT_FEATURED_KEY
+} from "../../components/plugin/defaults";
 
 
 export default class CommonBlock extends Component {
@@ -27,13 +31,25 @@ export default class CommonBlock extends Component {
 
   render(){
     const data = this.props.data;
+    const options = this.props.blockProps.options;
+    let defaultFeatured = DEFAULT_FEATURED_KEY;
+    let featuredOptions = DEFAULT_FEATURED_OPTIONS;
+
+    if (options) {
+      if (options.defaultFeatured) {
+        defaultFeatured = options.defaultFeatured;
+      }
+      if (options.featuredOptions) {
+        featuredOptions = options.featuredOptions;
+      }
+    }
 
     return (
       <BlockWrapper>
         <BlockControls>
           <Dropdown
-            items={this.props.featuredOptions}
-            selected={data.featured || this.props.defaultFeatured}
+            items={featuredOptions}
+            selected={data.featured || defaultFeatured}
             onChange={this._handleFeaturedChange} />
 
           <BlockActionGroup items={this.props.actions} />
